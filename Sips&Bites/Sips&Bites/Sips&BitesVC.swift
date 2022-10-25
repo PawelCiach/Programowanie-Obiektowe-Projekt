2e67291483fee4cf3cd84a27eff31888c4cba4b6
PawelCiach <115493184+PawelCiach@users.noreply.github.com>
2022-10-25 15:59:21 +0200

    Add files via upload

---
+++ Sips&Bites/Sips&Bites/Sips&BitesVC.swift
@@ -0,0 +1,20 @@
+//
+//  Sips&BitesVC.swift
+//  Sips&Bites
+//
+//  Created by Paweł Ciach on 24/10/2022.
+//
+
+import UIKit
+
+class Sips_BitesVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any){
+        performSegue(withIdentifier: "toMenuVC", sender: nil)
+    }
+}
---
+++ Sips&Bites/Sips&Bites/daniaVC.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class daniaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
