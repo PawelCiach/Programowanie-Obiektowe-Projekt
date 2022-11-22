b82598e12428dfc386f3142f262dd575474a9279
PawelCiach <268543@studet.pwr.edu.pl>
2022-11-22 16:38:27 +0100

    brak postępu

--- Sips&Bites/Sips&Bites/VCs/daniaVC.swift
+++ Sips&Bites/Sips&Bites/VCs/daniaVC.swift
@@ -21,17 +21,15 @@ class daniaVC: UIViewController{
 extension daniaVC: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
-      //return dania.count
+        return dania.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //guard let cell = tableView.dequeueReusableCell(withIdentifier: "danieCell")else{
-          //  return UITableViewCell()
-        //}
-        //cell.textLabel!.text = dania[indexPath.row]
-        //return cell
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "danieCell")else{
+          return UITableViewCell()
+        }
+        cell.textLabel!.text = dania[indexPath.row]
+        return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toDanieVC", sender: nil)
