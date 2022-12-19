0653d4d08381364b60ffdfc8c2d8c6c01100eb6f
MBichajlo <maciej.bichajlo.mb@gmail.com>
2022-12-19 16:10:04 +0100

    odczyt składników - collection view

--- Sips&Bites/Sips&Bites/VCs/danieVC.swift
+++ Sips&Bites/Sips&Bites/VCs/danieVC.swift
@@ -2,10 +2,12 @@
 import UIKit
 import CoreData
 
-class danieVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class danieVC: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
     
     
     
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     @IBOutlet weak var nazwaD: UILabel!
     
@@ -20,32 +22,6 @@ class danieVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
         performSegue(withIdentifier: "backToDaniaVC", sender: self)
     }
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-            return danie?.skladniki?.count ?? 1
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "abx") else{
-            return UITableViewCell()
-        }
-        
-        //cell.textLabel?.text=danie?.skladniki![indexPath.row].nazwaSkladnika
-        //cell.textLabel?.text=danie?.iloscSkladnikow![indexPath.row]
-        //cell.textLabel?.text=skladniki[indexPath.row].nazwaSkladnika
-        //cell.textLabel?.text=skladniki[indexPath.row].nazwaSkladnika
-        cell.textLabel?.text=skladniki[indexPath.row].nazwaSkladnika! + " - "+(danie?.iloscSkladnikow![indexPath.row])!
-        return cell
-            
-    }
-    
-    
-
-    
-   
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
@@ -58,8 +34,8 @@ class danieVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate=self
-        tableView.dataSource=self
+        collectionView.delegate=self
+        collectionView.dataSource=self
         //print(danie?.iloscSkladnikow as Any)
         //print(danie?.skladniki as Any)
         skladniki=danie?.skladniki?.array as! [Skladnik]
@@ -78,14 +54,34 @@ class danieVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     }
 }
 
-/*extension Optional where Wrapped == NSOrderedSet {
-    func array<T: Hashable>(of: T.Type) -> [T] {
-        if let set = self as? Set<T> {
-            return Array(set)
-        }
-        return [T]()
+extension danieVC:UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.skladniki.count
     }
-}*/
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "skladnikDanieCell", for: indexPath) as? skladnikCell else{
+            return UICollectionViewCell()
+        }
+        cell.configure(with: skladniki[indexPath.row], and: (danie?.iloscSkladnikow![indexPath.row])!)
+        cell.backgroundColor = UIColor.white
+        cell.layer.borderWidth=1
+        cell.layer.borderColor=UIColor.clear.cgColor
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //let label = UILabel(frame: CGRect.zero)
+        //label.text = skladnikiAkt[indexPath.row].name+" - "+skladnikiAkt[indexPath.row].ilosc
+        //label.sizeToFit()
+        //return CGSize(width: skladnikiAkt[indexPath.row].name.size(withAttributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17)]).width + 25, height: 30)
+        collectionView.layer.borderWidth=3
+        collectionView.layer.backgroundColor=UIColor.lightGray.cgColor
+        collectionView.layer.borderColor=UIColor.darkGray.cgColor
+        collectionView.layer.cornerRadius=7
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "skladnikDanieCell", for: indexPath) as! skladnikCell
+        cell.configure(with: skladniki[indexPath.row], and: (danie?.iloscSkladnikow![indexPath.row])!)
+        return CGSize(width:cell.nazwa.text!.size(withAttributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14)]).width + 8 , height: 30)
+    }
+}
 
 
